110462947ab68c73a4ecbe1ae918da8457f92e52
d11416214-dotcom <d11416214@gs.takming.edu.tw>
2026-03-27 11:01:01 +0800

    Add files via upload

---
+++ D11416214_H4-3/D11416214_H4-3/D11416214_H4-3/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class MyPoint
+{
+	private int x, y;
+	public int X
+	{
+		get
+		{
+			return x;
+		}
+		set
+		{
+			x = value;
+		}
+	}
+	public int Y
+	{
+		get
+		{
+			return y;
+		}
+		set
+		{
+			y= value;
+		}
+	}
+		
+}
---
+++ D11416214_H4-3/D11416214_H4-3/D11416214_H4-3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace D11416214_H4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyPoint pt= new MyPoint{X=10,Y=20 };
+
+            Console.WriteLine(pt.Y.ToString());
+            Console.WriteLine(pt.X.ToString());
+        }
+    }
+}
